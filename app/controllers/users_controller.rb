ed7b81013b888ddb10e93229c60b48e05110dcff
Daimeon Pilcher <daimeonpilcher@Daimeons-MacBook-Pro.local>
2015-11-10 12:53:52 -0700

    removed extra commas

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -69,6 +69,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:first_name,, :last_name,, :email,, :social_security_number)
+      params.require(:user).permit(:first_name, :last_name, :email, :social_security_number)
     end
 end
